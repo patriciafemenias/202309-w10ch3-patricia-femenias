691f8f38d9ea715906bde5d029dce587ec517b36
Patricia <patricia.femenias@gmail.com>
2023-11-16 23:09:51 +0100

    Add FurbysController test

---
+++ src/features/furbys/mocks/furbysMock.ts
@@ -0,0 +1,24 @@
+import { type FurbyStructure } from "../types";
+
+const furbysMock: FurbyStructure[] = [
+  {
+    id: "655528ebf122b4060c589a9e",
+    name: "Bumble Bee",
+    year: 1999,
+    type: "normal",
+  },
+  {
+    id: "655528ebf122b4060c589a9d",
+    name: "Leopard ",
+    year: 1998,
+    type: "normal",
+  },
+  {
+    id: "655528ebf122b4060c589a9f",
+    name: "Pink Flamingo",
+    year: 1998,
+    type: "normal",
+  },
+];
+
+export default furbysMock;
